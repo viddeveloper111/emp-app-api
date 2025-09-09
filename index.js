9045da8096572e34f4c9d01b6b96f290ec52a366
unknown <viddeveloper111@gmail.com>
2025-09-09 15:45:14 +0530

    cors

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 require("dotenv").config();
 const express = require("express");
+const cors = require("cors");   // ✅ import cors
 const connectDB = require("./config/db");
 const authRoutes = require("./routes/authRoutes");
 const organizationRoutes = require("./routes/organizationRoutes");
@@ -7,6 +8,8 @@ const leadRoutes = require("./routes/leadRoutes");
 
 const app = express();
 connectDB();
+
+app.use(cors()); 
 
 app.use(express.json());
 
@@ -16,4 +19,3 @@ app.use("/api/leads", leadRoutes);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
-
